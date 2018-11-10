c998c365cedcac97cafdbf552aba891ceb59d50f
David Wagner <wagdav@gmail.com>
2018-11-10 11:59:30 +0100

    Remove contact page

--- site.hs
+++ site.hs
@@ -24,7 +24,7 @@ main = hakyllWith config $ do
         route assets
         compile compressCssCompiler
 
-    match (fromList ["about.markdown", "contact.markdown"]) $ do
+    match (fromList ["about.markdown"]) $ do
         route cleanRoute
         compile
             $   pandocCompiler
