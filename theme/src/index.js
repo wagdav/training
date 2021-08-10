5b85b1f98115bef8e9f9b160e6b1b8eb08c399b7
David Wagner <wagdav@gmail.com>
2021-08-10 14:01:47 +0200

    Allow multiple GPX-widgets in one page

--- theme/src/index.js
+++ theme/src/index.js
@@ -8,30 +8,37 @@ import PinIconStart from 'leaflet-gpx/pin-icon-start.png';
 import PinIconEnd from 'leaflet-gpx/pin-icon-end.png';
 import PinShadow from 'leaflet-gpx/pin-shadow.png';
 
-var map = L.map('trace');
+function addTrace(element) {
+    var map = L.map(element);
+    var gpx = element.getAttribute("data-gpx-trace");
 
-map.addControl(new L.Control.Fullscreen({
-    pseudoFullscreen: true
-}));
+    map.addControl(new L.Control.Fullscreen({
+        pseudoFullscreen: true
+    }));
 
-var url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var attrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(url, {
-        minZoom: 8,
-        maxZoom: 16,
-        attribution: attrib
-    })
-    .addTo(map);
+    var url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var attrib = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(url, {
+            minZoom: 8,
+            maxZoom: 16,
+            attribution: attrib
+        })
+        .addTo(map);
 
-new L.GPX(document.getElementById("gpx").href, {
-        async: true,
-        marker_options: {
-            startIconUrl: PinIconStart,
-            endIconUrl: PinIconEnd,
-            shadowUrl: PinShadow,
-        },
-    })
-    .on('loaded', function(e) {
-        map.fitBounds(e.target.getBounds());
-    })
-    .addTo(map);
+    new L.GPX(gpx, {
+            async: true,
+            marker_options: {
+                startIconUrl: PinIconStart,
+                endIconUrl: PinIconEnd,
+                shadowUrl: PinShadow,
+            },
+        })
+        .on('loaded', function(e) {
+            map.fitBounds(e.target.getBounds());
+        })
+        .addTo(map);
+}
+
+Array
+    .from(document.getElementsByClassName("gpx-trace"))
+    .forEach(addTrace);
