0348dd6fbe770ee6146f2bbf1116101d225a8121
David Wagner <wagdav@gmail.com>
2026-03-02 08:59:41 +0100

    Update to nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The contents of https://training.thewagner.net";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }:
     let
