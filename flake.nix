b6c981d52a0a5e1927848fa5264ab9b1ed782096
David Wagner <wagdav@gmail.com>
2026-09-11 16:57:12 +0200

    Update hash

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
           yarnOfflineCache = pkgs.fetchYarnDeps {
             yarnLock = self + "/yarn.lock";
-            hash = "sha256-khMEJ/nFT3PZ+Fu1BlA3mJXgs1d3+Rcqn03nlCq/WZU=";
+            hash = "sha256-Vo0l1pSlrzpY/1XmpZX/IR5Z2EOqTLOUFV1JICSiwqI=";
           };
 
           nativeBuildInputs = [
